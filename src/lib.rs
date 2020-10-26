50adcda7a82611740f5b1f3053ee627d4b459c4c
Felix <me@mbilker.us>
2020-10-26 23:13:15 +0000

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use core::num::Wrapping;
 
 pub struct Arc4<'a> {
     key: &'a [u8],
-    state: [u8; 256], 
+    state: [u8; 256],
 
     i: u8,
     j: u8,
@@ -13,7 +13,7 @@ impl<'a> Arc4<'a> {
     pub fn with_key(key: &'a [u8]) -> Self {
         let mut s = Self {
             key: key,
-            state: [0;256],
+            state: [0; 256],
             i: 0,
             j: 0,
         };
@@ -25,11 +25,13 @@ impl<'a> Arc4<'a> {
     pub fn prga(&mut self, out: &mut [u8]) {
         let mut i = Wrapping(self.i);
         let mut j = Wrapping(self.j);
-        for z in 0 .. out.len() {
+        for z in 0..out.len() {
             i = i + Wrapping(1);
             j = j + Wrapping(self.state[i.0 as usize]);
             self.state.swap(i.0 as usize, j.0 as usize);
-            out[z] = self.state[(Wrapping(self.state[i.0 as usize]) + Wrapping(self.state[j.0 as usize])).0 as usize];
+            out[z] = self.state[(Wrapping(self.state[i.0 as usize])
+                + Wrapping(self.state[j.0 as usize]))
+            .0 as usize];
         }
 
         self.i = i.0;
@@ -39,11 +41,13 @@ impl<'a> Arc4<'a> {
     pub fn encrypt(&mut self, data: &mut [u8]) {
         let mut i = Wrapping(self.i);
         let mut j = Wrapping(self.j);
-        for z in 0 .. data.len() {
+        for z in 0..data.len() {
             i = i + Wrapping(1);
             j = j + Wrapping(self.state[i.0 as usize]);
             self.state.swap(i.0 as usize, j.0 as usize);
-            data[z] ^= self.state[(Wrapping(self.state[i.0 as usize]) + Wrapping(self.state[j.0 as usize])).0 as usize];
+            data[z] ^= self.state[(Wrapping(self.state[i.0 as usize])
+                + Wrapping(self.state[j.0 as usize]))
+            .0 as usize];
         }
 
         self.i = i.0;
@@ -52,12 +56,12 @@ impl<'a> Arc4<'a> {
 }
 
 fn ksa(s: &mut [u8], key: &[u8]) {
-    for i in 0 ..= 255 {
+    for i in 0..=255 {
         s[i] = i as u8;
     }
 
     let mut j = 0;
-    for i in 0 ..= 255 {
+    for i in 0..=255 {
         j = (Wrapping(j) + Wrapping(s[i]) + Wrapping(key[i % key.len()])).0;
         s.swap(i, j as usize);
     }
